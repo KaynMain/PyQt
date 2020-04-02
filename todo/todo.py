3273b5c2a206848e29eca990d4055f4d63d5990f
KaynMain <kacperkwitek@wp.pl>
2020-04-02 20:34:35 +0200

    Okno logowania użytkownika

--- todo/todo.py
+++ todo/todo.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QMessageBox, QInputDialog
-from gui import Ui_Widget
+from PyQt5.QtWidgets import QApplication, QWidget
+from gui import Ui_Widget, LoginDialog
 
 
 class Zadania(QWidget, Ui_Widget):
@@ -14,23 +14,22 @@ class Zadania(QWidget, Ui_Widget):
         self.setupUi(self)
         self.logujBtn.clicked.connect(self.loguj)
         self.koniecBtn.clicked.connect(self.koniec)
-        
-    def loguj(self):
-        login, ok = QInputDialog.getText(self, 'Logowanie', 'Podaj login:')
-        if ok:
-            haslo, ok = QInputDialog.getText(self, 'Logowanie', 'Podaj haslo:')
-            if ok:
-                if not login or not haslo:
-                    QMessageBox.warning(
-                        self, 'Błąd', 'Pusty login lub hasło!', QMessageBox.Ok)
-                    return
-                QMessageBox.information(
-                    self, 'Dane logowania',
-                    'Podano: ' + login + ' ' + haslo, QMessageBox.Ok)
 
+    def loguj(self):
+        login, haslo, ok = LoginDialog.getLoginHaslo(self)
+        if not ok:
+            return
+
+        if not login or not haslo:
+            QMessageBox.warning(self, 'Błąd',
+                                'Pusty login lub hasło', QMessageBox.Ok)
+            return
+        
+        QMessageBox.information(self, 'Dane logowania', 'Podano: ' +
+                                login + ' ' + haslo, QMessageBox.Ok)
+                                
     def koniec(self):
         self.close()
-
 
 if __name__ == '__main__':
     import sys
